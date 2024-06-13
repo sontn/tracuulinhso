c5394c417af9c0c84f4a5f9650783f016207bdd8
Son Tran <son.tranngoc@gmail.com>
2024-06-13 15:27:57 +0700

    Add list dai cat

---
+++ read_csv.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+from check_phone_number_array import tam_mot_linh_so_func
+
+file_path = '/home/ws0561/Downloads/DuongDz_2.csv'
+
+df = pd.read_csv(file_path, header=None, names=['PhoneNumber'])
+
+for number in df['PhoneNumber']:
+    tam_mot_linh_so_func(str(number))
